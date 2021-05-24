2cd5c507ee3080d9ecf2c16516120b9fe55098ad
Mevan <mevanabeydeera@gmail.com>
2021-05-24 05:38:22 +0400

    Add: Main page & global styles

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home = () => (
+    <div className="container">
+      <Head>
+        <title>Metamask Connection Demo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+        By Mevan Abeydeera
+      </footer>
+    </div>
+  );
+
+export default Home;
