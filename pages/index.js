820b78203b9e0c5934c0d05e47c5a2da4e7f2053
Mevan <mevanabeydeera@gmail.com>
2021-05-24 10:10:12 +0400

    Update: Index with Moralis provider

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
+import { MoralisProvider } from 'react-moralis';
+
+import { MORALIS_APP_ID, MORALIS_SERVER_URL } from 'shared/constants';
+import { Header, Footer, MetaMask } from 'components';
 
 const Home = () => (
-    <div className="container">
-      <Head>
-        <title>Metamask Connection Demo</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MoralisProvider appId={MORALIS_APP_ID} serverUrl={MORALIS_SERVER_URL}>
+      <div className="container">
+        <Head>
+          <title>Metamask Connection Demo</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
+        <main>
+          <Header />
+          <MetaMask />
+        </main>
 
-      </main>
-
-      <footer>
-        By Mevan Abeydeera
-      </footer>
-    </div>
+        <Footer>
+          By Mevan Abeydeera
+        </Footer>
+      </div>
+    </MoralisProvider>
   );
 
 export default Home;
