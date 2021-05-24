9c7e435a9a323f3a31e9137ab40a410ad8943d86
Mevan <mevanabeydeera@gmail.com>
2021-05-24 11:02:06 +0400

    Update: users Primary EthAdress

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useMoralis } from 'react-moralis';
 
+import { Button } from 'components';
 import styles from './Header.module.scss';
 
-const Header = ({ children }) => <header className={styles.container}>{children}</header>;
+const Header = () => {
+  const { authenticate, isAuthenticated, logout, user } = useMoralis();
+
+  const primaryEthAddress = useMemo(() => isAuthenticated && (user.get('accounts')[0]), [isAuthenticated, user]);
+
+  return (
+    <header className={styles.container}>
+      <div>
+        {!isAuthenticated && (
+          <div>
+            <Button onClick={() => authenticate()}>Authenticate</Button>
+          </div>
+        )}
+        {isAuthenticated && (
+          <div>
+            {primaryEthAddress}
+            <Button onClick={() => logout()}>Logout</Button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
 
 Header.defaultProps = {
+  children: null,
 };
 
 Header.propTypes = {
