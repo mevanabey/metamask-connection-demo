9162e06c7850815d719816a8bae7831c7f9c4bda
Mevan <mevanabeydeera@gmail.com>
2021-05-24 10:40:32 +0400

    Update: Button component to support onClick

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 import styles from './Button.module.scss';
 
-const Button = ({ children }) => <button className={styles.button}>{children}</button>;
+const Button = ({ children, onClick }) => <button className={styles.button} onClick={onClick}>{children}</button>;
 
 Button.defaultProps = {
   children: null,
+  onClick: null,
 };
 
 Button.propTypes = {
   children: PropTypes.node,
+  onClick: PropTypes.func,
 };
 
 export default Button;
