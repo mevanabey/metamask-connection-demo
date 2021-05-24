7ee33a2c8e3e9a04ca4f1c20c26093100e7c5be9
Mevan <mevanabeydeera@gmail.com>
2021-05-24 05:52:24 +0400

    Add: constants

---
+++ shared/constants.js
@@ -0,0 +1,8 @@
+// General
+export const WEI_IN_WTH = 1000000000000000000;
+export const ETHERSCAN_API_URL = 'https://api.etherscan.io/api?module=account';
+
+//Move to Env Variables (Private)
+export const MORALIS_APP_ID = 'TCDv7lnGwNN7RIhlj30wA69dnaamxtsmqcTnm3ch';
+export const MORALIS_SERVER_URL = 'https://9c8nrl6lfq8u.moralis.io:2053/server';
+export const ETHERSCAN_API_KEY = 'WY7V2HASZ7D2QR46JR2C9S8Q49ZC9FS4WT';
