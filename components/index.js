693ffcb2d445b3538d11d923e97e8b41976be0c9
Mevan <mevanabeydeera@gmail.com>
2021-05-24 10:07:23 +0400

    Add: Index for better component import

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import MetaMask from 'components/MetaMask/MetaMask';
+import Header from 'components/Layout/Header/Header';
+import Footer from 'components/Layout/Footer/Footer';
+
+export {
+  MetaMask,
+  Header,
+  Footer
+};
